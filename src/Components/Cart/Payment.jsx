1965a0b33543a0f5861d4d60d5d4935972e1443a
Kanishk Pareek <knshkpareek@gmail.com>
2024-04-03 21:40:48 +0530

    changes

--- src/Components/Cart/Payment.jsx
+++ src/Components/Cart/Payment.jsx
@@ -9,7 +9,7 @@ function PaymentScreen() {
     // Logic to handle payment based on the selected method
     switch(method) {
       case 'phonepe':
-        window.open('upi://pay?pa=9166033002@paytm&pn=Ashu&cu=INR');
+        window.open('upi://pay?pa=9166033002@ybl&pn=Ashu&cu=INR');
         break;
       case 'paytm':
         window.open('upi://pay?pa=9166033002@paytm&pn=Ashu&cu=INR');
@@ -36,24 +36,7 @@ function PaymentScreen() {
         height:' 50px' }} />
           PhonePe
         </Button>
-        <Button
-          variant="contained"
-          color="primary"
-          style={{ fontWeight: 'bold', padding: '10px 20px',background: '#fff',color:"#3498db"}}
-          onClick={() => handlePayment('paytm')}
-        >
-          <img src={PaytmLogo} alt="Paytm Logo" style={{ width: '50px',height:'50px', marginBottom: '10px' }} />
-          Paytm
-        </Button>
-        <Button
-          variant="contained"
-          color="primary"
-          style={{ fontWeight: 'bold', padding: '10px 20px',background: '#fff',color:"#3498db"}}
-          onClick={() => handlePayment('googlepay')}
-        >
-          <img src={GooglePayLogo} alt="Google Pay Logo" style={{ width: '50px', marginBottom: '10px' ,height:'50px'}} />
-          Google Pay
-        </Button>
+
       </div>
     </div>
   );
